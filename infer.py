77f120d263cb3cdef2a881954393706913d41797
datou30 <datou30@mail.ustc.edu.cn>
2024-07-10 12:36:59 +0800

    Add files to softrank branch

--- infer.py
+++ infer.py
@@ -7,133 +7,153 @@ from model.encoder import Encoder
 from model.infonet import infonet
 from model.query import Query_Gen_transformer
 from scipy.stats import rankdata
+import lightning
+import hydra
+import torchsort
+from einops import rearrange
 
 global device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def load_config(config_path):
-    with open(config_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+class infonet_lightning(lightning.LightningModule):
+    def __init__(self):
+        super().__init__()
+        encoder = Encoder(
+            input_dim=input_dim,
+            latent_num=latent_num,
+            latent_dim=latent_dim,
+            cross_attn_heads=8,
+            self_attn_heads=16,
+            num_self_attn_per_block=8,
+            num_self_attn_blocks=1,
+        )
 
-def create_model(config):
-    encoder = Encoder(
-        input_dim=config['model']['input_dim'],
-        latent_num=config['model']['latent_num'],
-        latent_dim=config['model']['latent_dim'],
-        cross_attn_heads=config['model']['cross_attn_heads'],
-        self_attn_heads=config['model']['self_attn_heads'],
-        num_self_attn_per_block=config['model']['num_self_attn_per_block'],
-        num_self_attn_blocks=config['model']['num_self_attn_blocks']
-    )
+        decoder = Decoder(
+            q_dim=decoder_query_dim,
+            latent_dim=latent_dim,
+        )
 
-    decoder = Decoder(
-        q_dim=config['model']['decoder_query_dim'],
-        latent_dim=config['model']['latent_dim'],
-    )
+        query_gen = Query_Gen_transformer(
+            input_dim=input_dim,
+            dim=decoder_query_dim,
+        )
+        self.model = infonet(encoder=encoder, decoder=decoder, query_gen=query_gen, decoder_query_dim=decoder_query_dim)
 
-    query_gen = Query_Gen_transformer(
-        input_dim=config['model']['input_dim'],
-        dim=config['model']['decoder_query_dim']
-    )
+    def forward(self, x):
+        return self.model(x)
+
+def load_model(config_path: str, config_name: str, ckpt_path: str) -> infonet_lightning:
     
-    model = infonet(
-        encoder=encoder,
-        decoder=decoder,
-        query_gen=query_gen,
-        decoder_query_dim=config['model']['decoder_query_dim']
-    ).to(device)
-    
+    hydra.initialize(config_path=config_path)
+    cfg = hydra.compose(config_name=config_name)
+
+    global latent_dim, latent_num, input_dim, batchsize, seq_len, decoder_query_dim, max_input_dim
+    batchsize = cfg.batchsize
+    latent_dim = cfg.latent_dim
+    latent_num = cfg.latent_num
+    input_dim = cfg.input_dim
+    seq_len = cfg.seq_len
+    decoder_query_dim = cfg.decoder_query_dim
+
+    torch.set_float32_matmul_precision('medium')
+
+    model = infonet_lightning.load_from_checkpoint(
+        ckpt_path,
+        map_location=lambda storage, loc: storage.cuda(0)
+    )
+
     return model
 
-def load_model(config_path, checkpoint_path):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    config = load_config(config_path)
-    model = create_model(config)
-    model.load_state_dict(torch.load(checkpoint_path, map_location=device))
-    model.eval()
-    return model
+def softrank_preprocessing(input_tensor, regularization_strength=0.1):
+    ## x has shape [batchsize, seq_len, 2]
+    min_val = torch.min(input_tensor, dim=1, keepdim=True).values
+    max_val = torch.max(input_tensor, dim=1, keepdim=True).values
+    scaled_tensor = (input_tensor - min_val) / (max_val - min_val)
+    
+    softrank = torchsort.soft_rank(rearrange(scaled_tensor, 'b n d -> (d b) n'), regularization_strength=regularization_strength)
+    softrank = rearrange(softrank, '(d b) n -> b n d', d=2)
 
-def estimate_mi(model, x, y):
+    min_val = torch.min(softrank, dim=1, keepdim=True).values
+    max_val = torch.max(softrank, dim=1, keepdim=True).values
+    scaled_softrank = (softrank - min_val) / (max_val - min_val) 
+
+    return scaled_softrank
+
+def estimate_mi(module, x, y):
     ## x and y are 1 dimensional sequences
-    model.eval()
-    x = rankdata(x)/len(x)
-    y = rankdata(y)/len(y)
+    module.eval()
     batch = torch.stack((torch.tensor(x, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)), dim=1).unsqueeze(0).to(device) ## batch has shape [1, sequence length, 2]
+    batch = softrank_preprocessing(batch)
     with torch.no_grad():
-        mi_lb = model(batch)
+        mi_lb = module(batch)
     return mi_lb
 
-def infer(model, batch):
-    ### batch has shape [batchsize, seq_len, 2]
-    model.eval()
-    batch = torch.tensor(batch, dtype=torch.float32, device=device)
-    with torch.no_grad():
-
-        mi_lb = model(batch)
-        MI = torch.mean(mi_lb)
-
-    return MI.cpu().numpy()
-
-def compute_smi_mean(sample_x, sample_y, model, proj_num, seq_len, batchsize):
+def compute_smi_mean(sample_x, sample_y, module, proj_num, batchsize):
     ## we use sliced mutual information to estimate high dimensional correlation
     ## proj_num means the number of random projections you want to use, the larger the more accuracy but higher time cost
     ## seq_len means the number of samples used for the estimation
     ## batchsize means the number of one-dimensional pairs estimate at one time, this only influences the estimation speed
+    module.eval()
     dx = sample_x.shape[1]
     dy = sample_y.shape[1]
+    seq_len = sample_x.shape[0]
     results = []
     for i in range(proj_num//batchsize):
-        batch = np.zeros((batchsize, seq_len, 2))
-        for j in range(batchsize):
-            theta = np.random.randn(dx)
-            phi = np.random.randn(dy)
-            x_proj = np.dot(sample_x, theta)
-            y_proj = np.dot(sample_y, phi)
-            x_proj = rankdata(x_proj)/seq_len
-            y_proj = rankdata(y_proj)/seq_len
-            xy = np.column_stack((x_proj, y_proj))
-            batch[j, :, :] = xy
-        infer1 = infer(model, batch)
-        mean_infer1 = np.mean(infer1)
+        
+        batch = torch.zeros((batchsize, seq_len, 2)).to(device)
+        theta = torch.randn(batchsize, dx).to(device)  
+        phi = torch.randn(batchsize, dy).to(device)   
+
+        x_proj = torch.matmul(sample_x, theta.T)  # [seq_len, batchsize]
+        y_proj = torch.matmul(sample_y, phi.T)    # [seq_len, batchsize]
+
+        x_proj = x_proj.permute(1, 0)  # [batchsize, seq_len]
+        y_proj = y_proj.permute(1, 0)  # [batchsize, seq_len]
+
+        batch = torch.stack((x_proj, y_proj), dim=2)  # [batchsize, seq_len, 2]
+        batch = softrank_preprocessing(batch)
+        with torch.no_grad():
+            infer1 = module(batch)
+        mean_infer1 = torch.mean(infer1)
         results.append(mean_infer1)
 
-    return np.mean(np.array(results))
+    return torch.mean(torch.tensor(results))
 
-def example_d_1():
+def example_d_1(module):
+
+    module.eval()
     seq_len = 4781
     results = []
     real_MIs = []
     
     for rou in np.arange(-0.9, 1, 0.1):
-        x, y = np.random.multivariate_normal(mean=[0,0], cov=[[1,rou],[rou,1]], size=seq_len).T
-        x = rankdata(x)/seq_len #### important, data preprocessing is needed, using rankdata(x)/seq_len to map x and y to [0,1]
-        y = rankdata(y)/seq_len
-        result = estimate_mi(model, x, y).squeeze().cpu().numpy()
+        sample = np.random.multivariate_normal(mean=[0,0], cov=[[1,rou],[rou,1]], size=seq_len).T.astype(np.float32)
+        sample = torch.from_numpy(sample).unsqueeze(0)
+        sample = rearrange(sample, 'b d n -> b n d')
+        sample = softrank_preprocessing(sample).to(device)
+        with torch.no_grad():
+            result = module(sample).squeeze().cpu().numpy()
         real_MI = -np.log(1-rou**2)/2
         real_MIs.append(real_MI)
         results.append(result)
         print("estimate mutual information is: ", result, "real MI is ", real_MI  )
 
-def example_highd():
+def example_highd(module):
     d = 10
     mu = np.zeros(d)
     sigma = np.eye(d)
-    sample_x = np.random.multivariate_normal(mu, sigma, 2000)
-    sample_y = np.random.multivariate_normal(mu, sigma, 2000)
-    result = compute_smi_mean(sample_x, sample_y, model, seq_len=2000, proj_num=1024, batchsize=32)
+    sample_x = torch.from_numpy(np.random.multivariate_normal(mu, sigma, 2000).astype(np.float32)).to(device)
+    sample_y = torch.from_numpy(np.random.multivariate_normal(mu, sigma, 2000).astype(np.float32)).to(device)
+    result = compute_smi_mean(sample_x, sample_y, module, proj_num=1024, batchsize=64)
     print(f"result is {result}")
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='Load model from config and checkpoint')
-    parser.add_argument('--config', type=str, required=False, default='configs/config.yaml', help='Path to the config file')
-    parser.add_argument('--checkpoint', type=str, required=False, default="saved/uniform/model_5000_32_1000-720--0.16.pt", help='Path to the model checkpoint')
-    
-    args = parser.parse_args()
-    
-    model = load_model(args.config, args.checkpoint)
+    config_path = 'configs'
+    config_name = 'cfg_softrank_0.1_new'
+    ckpt_path = '/home/hzy/work/kNN/infonet_softrank/saved/basic_hypernet-epoch=1399-val_acc=98.93.ckpt'
+    model = load_model(config_path, config_name, ckpt_path)
     print("Model loaded successfully")
-
-    example_d_1()
-    #example_highd()
+    example_d_1(model)
+    #example_highd(model)
+    #x, y = np.random.multivariate_normal(mean=[0,0], cov=[[1,0.6],[0.6,1]], size=seq_len).T.astype(np.float32)
+    #mi = estimate_mi(model, x, y)
---
+++ train_softrank.py
@@ -0,0 +1,304 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchsort
+import torch.autograd as autograd
+from data import gen_train_data
+from model.decoder import Decoder
+from model.encoder import Encoder
+from model.infonet import infonet
+from model.query import Query_Gen_transformer
+from scipy.stats import rankdata
+from tensorboardX import SummaryWriter
+from torch import nn, optim
+from sklearn.mixture import GaussianMixture
+import pickle
+
+#import tensorrt as trt
+import matplotlib.pyplot as plt
+import numpy as np
+from lightning.pytorch.callbacks import ModelCheckpoint
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS
+
+from data import gen_train_data, gen_gauss_xyz, gen_train_data_softrank_weighted, gen_train_data_softrank_weighted_pre
+from model.decoder import Decoder 
+from model.encoder import Encoder 
+from model.infonet import infonet
+from model.query import Query_Gen_transformer
+from scipy.stats import rankdata
+from torch.optim import Adam
+import lightning
+from lightning.pytorch.loggers import TensorBoardLogger
+from send2trash import send2trash
+import hydra
+from hydra.core.hydra_config import HydraConfig
+from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import CosineAnnealingLR
+torch.backends.cuda.enable_flash_sdp(True)
+
+learning_rate = 1e-4
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class InfoNetDataset(torch.utils.data.Dataset):
+    def __init__(self, total_epoch):
+        super().__init__()
+        self.total_epoch = total_epoch
+
+    def __len__(self):
+        return self.total_epoch
+
+    def __getitem__(self, idx):
+        res = gen_train_data_softrank_weighted_pre(batchsize=1, seq_len=seq_len, dim=1, com_num=10, reg=reg).squeeze(0)
+        return res
+
+
+class LightningWrapper(lightning.LightningModule):
+    def __init__(self):
+        super().__init__()
+        encoder = Encoder(
+            input_dim=input_dim,
+            latent_num=latent_num,
+            latent_dim=latent_dim,
+            cross_attn_heads=8,
+            self_attn_heads=16,
+            num_self_attn_per_block=8,
+            num_self_attn_blocks=1,
+        )
+
+        decoder = Decoder(
+            q_dim=decoder_query_dim,
+            latent_dim=latent_dim,
+        )
+
+        query_gen = Query_Gen_transformer(
+            input_dim = input_dim,
+            dim = decoder_query_dim,
+        )
+        self.model = infonet(encoder=encoder, decoder=decoder, query_gen=query_gen,
+                                 decoder_query_dim=decoder_query_dim)
+
+    def forward(self, x):
+        #total_params = sum(p.numel() for p in self.model.parameters())
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        mi_lb = self(batch)
+        loss = -torch.mean(mi_lb)
+        self.logger.experiment.add_scalars('train_loss', 
+                                           {f'dim{input_dim//2}': loss,
+                                            },
+                                           global_step=self.global_step)
+        self.log('loss', loss, on_step=True, prog_bar=True, sync_dist=True, batch_size=batchsize)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        
+        dim_list = [1, 2, 5, 10]
+        for dim in dim_list:
+            eval_acc = gmm_order_eval(self.model, dim, seq_len=5000, number_test=500, iter=self.current_epoch + 1, reg=reg)
+            self.logger.experiment.add_scalars('val_acc', 
+                                {f'dim{dim}': eval_acc}, 
+                                global_step=self.global_step)
+        self.log(f'val_acc', eval_acc, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        return eval_acc
+
+    def test_step(self, batch, batch_idx):
+        return
+
+    def configure_optimizers(self):
+        optimizer = Adam(self.parameters(), lr=learning_rate)
+        scheduler = StepLR(optimizer, step_size=8000, gamma=0.9)
+        #scheduler = CosineAnnealingLR(optimizer, T_max=10000, eta_min=0)
+        
+        return {"optimizer": optimizer, "lr_scheduler": {"scheduler": scheduler, "interval": "step"}}
+    
+    
+    def on_load_checkpoint(self, checkpoint):
+        if "optimizer_states" in checkpoint:
+            for state in checkpoint["optimizer_states"]:
+                for param_group in state['param_groups']:
+                    print("Loaded learning rate:", param_group['lr'])
+                    param_group['lr'] = learning_rate
+
+    def train_dataloader(self):
+        dataset = InfoNetDataset(total_epoch=4500)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=15, shuffle=True, num_workers=4, pin_memory=True)
+        return dataloader
+
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(InfoNetDataset(total_epoch=1), batch_size=1)
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(InfoNetDataset(total_epoch=1), batch_size=1)
+
+def compare(list_a, list_b):
+    return [1 if a >= b else 0 for a, b in zip(list_a, list_b)]
+
+def gmm_order_eval(module, dim, iter, seq_len=2000, number_test=500, reg=0.1):
+    
+    module.eval()
+    path = "/home/hzy/work/kNN/InfoNet-main-V2/num_compare"
+
+    full_path = os.path.join(path, f'num_{dim}')
+    MI_XY = []
+    MI_XZ = []
+    results_xy = []
+    results_xz = []
+
+    with open(os.path.join(full_path, "weight.pkl"), 'rb') as f:
+        weights = pickle.load(f)
+
+    with open(os.path.join(full_path, "mean.pkl"), 'rb') as f:
+        means = pickle.load(f)
+
+    with open(os.path.join(full_path, "cov.pkl"), 'rb') as f:
+        covs = pickle.load(f)
+
+    with open(os.path.join(full_path, "XY_mi_gauss.txt"), 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            value = float(line.strip())
+            MI_XY.append(value)
+
+    with open(os.path.join(full_path, "XZ_mi_gauss.txt"), 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            value = float(line.strip())
+            MI_XZ.append(value)
+
+    for idx in range(number_test):
+            
+        if idx>=len(weights):
+            continue
+
+        gm = GaussianMixture(n_components=5)
+        gm.weights_ = np.array(weights[idx])
+        gm.means_ = np.array(means[idx])
+        gm.covariances_ = np.array(covs[idx])
+            
+        data, labels = gm.sample(n_samples=seq_len)
+
+        data = np.array(data)
+        for row in range(3):
+            ###
+            tmp = torch.from_numpy(data[:, row])
+            tmp = (tmp - torch.min(tmp))/(torch.max(tmp) - torch.min(tmp))
+            ###
+            tmp = tmp.unsqueeze(0)
+            tmp = torchsort.soft_rank(tmp, regularization_strength=reg).squeeze(0).numpy()
+            tmp = (tmp - np.min(tmp))/(np.max(tmp) - np.min(tmp))
+            data[:, row] = tmp
+
+        data_xy = torch.from_numpy(data[:, [0, 1]]).unsqueeze(0).type(torch.float32).to(device)
+        data_xz = torch.from_numpy(data[:, [0, 2]]).unsqueeze(0).type(torch.float32).to(device)
+        with torch.no_grad():
+            infer_1 = module(data_xy).cpu().numpy()
+            infer_2 = module(data_xz).cpu().numpy()
+            
+        results_xy.append(infer_1)
+        results_xz.append(infer_2)
+        
+    comp = compare(MI_XY[:number_test], MI_XZ[:number_test])
+    comp_c = compare(results_xy, results_xz)
+
+    accuracy_c = 0
+    for j in range(len(comp_c)):
+        if comp[j] == comp_c[j]:
+            accuracy_c+=1
+    acc_rate = accuracy_c / len(comp) * 100
+    #print("==============================> order acc is:", accuracy_rate)
+
+    save_text_root_path = os.path.join(logger.log_dir, "gauss_order")
+    text_path = os.path.join(save_text_root_path, f"dim-{dim}")
+    if not os.path.exists(text_path):
+        os.makedirs(text_path, exist_ok=True)
+    with open(os.path.join(text_path, f"model-firsttry-{seq_len}.txt"), "a") as f:
+        output_string = f"order test acc on {dim}-dimension gauss of model_{iter} is {acc_rate}\n"
+        f.write(output_string)
+        print(output_string)
+
+    fig_root_path = os.path.join(logger.log_dir, 'figure')
+    os.makedirs(fig_root_path, exist_ok=True)
+    fig_save_cat_path = os.path.join(fig_root_path, f"{dim}")
+    os.makedirs(fig_save_cat_path, exist_ok=True)
+
+    plt.style.use("ggplot")
+    fig = plt.figure(figsize=(14, 10))
+
+    ax1 = fig.add_subplot(121)
+    d_draw = np.arange(number_test)
+
+    zipped_lists = sorted(zip(MI_XY[:number_test], results_xy[:number_test]), reverse=False)
+    sorted_list1, sorted_list2 = zip(*zipped_lists)
+    ax1.plot(d_draw[:50], MI_XY[:50], color="red", lw=2, ls="-", label="real mutual information XY", markersize=10)
+    ax1.plot(d_draw[:50], results_xy[:50], color="blue", lw=2, ls="-", label="estimate MI XY", markersize=10)
+    ax1.set_xlabel("# experiment times", fontweight="bold", fontsize=20)
+    ax1.set_ylabel(" mutual information ", fontweight="bold", fontsize=20)
+    # ax1.tick_params(axis='x', which='major', labelsize=14, width=2, length=6)
+    # ax1.tick_params(axis='y', which='major', labelsize=14, width=2, length=6)
+    ax1.legend(fontsize=20, loc="upper left")
+
+    ax2 = fig.add_subplot(122)
+    ax2.plot(d_draw, sorted_list1, color="red", lw=2, ls="-", label="real mutual information XY", markersize=10)
+    ax2.plot(d_draw, sorted_list2, color="blue", lw=2, ls="-", label="estimate MI XY", markersize=10)
+    ax2.set_xlabel("# experiment times", fontweight="bold", fontsize=20)
+    ax2.set_ylabel(" mutual information ", fontweight="bold", fontsize=20)
+    # ax1.tick_params(axis='x', which='major', labelsize=14, width=2, length=6)
+    # ax1.tick_params(axis='y', which='major', labelsize=14, width=2, length=6)
+    ax2.legend(fontsize=20, loc="upper left")
+
+    image_save_path = os.path.join(fig_save_cat_path, f"gauss-dim{dim}-iter{iter}.png")
+    plt.savefig(image_save_path)
+
+    return acc_rate
+
+@hydra.main(config_path='configs', config_name='cfg_softrank_0.1_new.yaml', version_base='1.1')
+def main(cfg):
+
+    global latent_dim, latent_num, input_dim, batchsize, seq_len, decoder_query_dim, reg
+    batchsize = cfg.batchsize
+    latent_dim = cfg.latent_dim
+    latent_num = cfg.latent_num
+    input_dim = cfg.input_dim
+    seq_len = cfg.seq_len
+    decoder_query_dim = cfg.decoder_query_dim
+    reg = cfg.reg
+    # print(f"batchsize: {batchsize}, latent_dim: {latent_dim}, latent_num: {latent_num}, \
+    #     input_dim: {input_dim}, seq_len: {seq_len}, decoder_query_dim: {decoder_query_dim}, version: {cfg.version}")
+    # gpu_id = cfg.device[HydraConfig.get().job.num % len(cfg.device)]
+    # print("gpu id:", gpu_id)
+
+    torch.set_float32_matmul_precision('medium')
+    module = LightningWrapper()
+
+    global logger
+    logger = TensorBoardLogger('logs', name=cfg.name, version=cfg.version)
+    #if os.path.isdir(logger.log_dir):
+        #send2trash(logger.log_dir)
+        
+    trainer = lightning.Trainer(max_epochs=1500,
+                                accelerator='auto',
+                                devices=[1,2,3],
+                                logger=logger,
+                                num_sanity_val_steps=0,
+                                strategy='ddp_find_unused_parameters_true',
+                                gradient_clip_val=1.0,
+                                check_val_every_n_epoch=40,
+                                # limit_val_batches=0,  # Disable validation for now as the script is not correct.
+                                callbacks=[
+                                    ModelCheckpoint(
+                                        monitor='val_acc',
+                                        dirpath=os.path.join(logger.log_dir, 'checkpoints'),
+                                        filename='basic_hypernet-{epoch:02d}-{val_acc:.2f}',
+                                        save_top_k=35,
+                                        mode='max',
+                                        every_n_epochs=40,
+                                        save_last=True
+                                    )
+                                ]
+                                )
+    trainer.fit(module)
+
+if __name__ == '__main__':
+    main()
